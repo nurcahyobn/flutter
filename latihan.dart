c7eae5206871a927248a3fb2cdd4cdd8a67357c5
Nurcahyo Budi Nugroho <nurcahyobn@gmail.com>
2021-08-24 18:14:32 +0700

    Create latihan.dart

---
+++ latihan.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Belajar Mobile1 - 5SIC3'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
